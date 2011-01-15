fe2fd90191574a35576a53d76bece150afdb30f3
Eric Merritt <ericbmerritt@gmail.com>
2011-01-14 21:04:37 -0600

    remove redundant documentation from wdoc

--- wdoc.el
+++ wdoc.el
@@ -1,15 +1,7 @@
-;;; wdoc.el --- hypertext authoring the WikiWay
+;;; wdoc.el --- Documentation authoring the WikiWay
 
 ;; Copyright (C) 2001, 2002  Alex Schroeder <alex@gnu.org>
-
-;; Emacs Lisp Archive Entry
-;; Filename: wdoc.el
-;; Version: 2.1.8
-;; Keywords: hypermedia
-;; Author: Alex Schroeder <alex@gnu.org>
-;; Maintainer: Alex Schroeder <alex@gnu.org>
-;; Description: Hypertext authoring the WikiWay
-;; URL: http://www.emacswiki.org/cgi-bin/wiki.pl?WikiMode
+;; Copyright (C) 2011 Eric Merritt <ericbmerritt@gmail.com>
 
 ;; This file is not part of GNU Emacs.
 
@@ -27,61 +19,6 @@
 ;; along with GNU Emacs; see the file COPYING.  If not, write to the
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ;; MA 02111-1307, USA.
-
-;;; Commentary:
-
-;; Wdoc is a hypertext and a content management system: Normal users are
-;; encouraged to enhance the hypertext by editing and refactoring
-;; existing pages and by adding more pages.  This is made easy by
-;; requiring a certain way of writing pages.  It is not as complicated
-;; as a markup language such as HTML.  The general idea is to write
-;; plain ASCII.  Word with mixed case such as ThisOne are WikiNames --
-;; they may be a Link or they may not.  If they are, clicking them will
-;; take you to the page with that WikiName; if they are not, clicking
-;; them will create an empty page for you to fill out.
-
-;; This mode does all of this for you without using a web browser, cgi
-;; scripts, databases, etc.  All you need is Emacs!  In order to
-;; install, put wdoc.el on you load-path, and add the following to your
-;; .emacs file:
-
-;; (require 'wdoc)
-
-;; This will activate WdocMode for all files in `wdoc-directories' as soon
-;; as they are opened.  This works by adding `wdoc-maybe' to
-;; `find-file-hooks'.
-
-;; Emacs provides the functionality usually found on Wiki web sites
-;; automatically: To find out how many pages have links to your page,
-;; use `grep' or `dired-do-search'.  To get an index of all wikis, use
-;; `dired'.  To keep old versions around, use `version-control' or use
-;; `vc-next-action'.  To edit wikis, use Emacs!
-
-;;; What about a Major Mode?
-
-;; By default, wiki files will be in `fundamental-mode'.  I prefer to be
-;; in `text-mode', instead.  You can do this either for all files that
-;; have WikiNames by changing `auto-mode-alist', or you can make
-;; text-mode the default mode instead of fundamental mode.  Example:
-
-;; (setq default-major-mode 'text-mode)
-
-;; This puts wiki files in `text-mode'.  One problem remains, however.
-;; Text mode usually means that the apostrophe is considered to be part
-;; of words, and some WikiNames will not be highlighted correctly, such
-;; as "WikiName''''s".  In that case, change the syntax table, if you
-;; don't mind the side effects.  Example:
-
-;; (modify-syntax-entry ?' "." text-mode-syntax-table)
-
-;;; Thanks
-
-;; Frank Gerhardt <Frank.Gerhardt@web.de>, author of the original wiki-mode.
-;;   His latest version is here: http://www.s.netic.de/fg/wiki-mode/wiki.el
-;; Thomas Link <t.link@gmx.at>
-;; John Wiegley <johnw@gnu.org>, author of emacs-wiki.el.
-;;   His latest version is here: http://www.gci-net.com/~johnw/Emacs/emacs-wiki.el
-
 
 
 ;;; Code:
